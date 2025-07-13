e1f45452acc8c9cbf31bdd646eca62de134a0d5f
creeksonjoseph <charanajoseph@gmail.com>
2025-07-13 11:29:05 +0300

    f

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header({ onDarkModeClick, isDarkMode }) {
+  return (
+    <>
+      <h2>Shopster</h2>
+      <button onClick={onDarkModeClick}>
+        {isDarkMode ? "Dark" : "Light"} Mode
+      </button>
+    </>
+  );
+}
+export default Header;
